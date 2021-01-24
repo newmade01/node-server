b3031156681a067d918152f38377e2341c93f157
newmade01 <newmade01@naver.com>
2021-01-24 21:14:56 +0900

    request

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const http = require("http");
 
 //들어옴, 응답
+//server->clinet로 text값 보냄
 const server = http.createServer((req, res) => {
-    console.log(req);
-})
+    // console.log(req.url); //url 설정
+    res.write("<h1>hello from nodejs")
+    res.end()
+});
 
 //localhost port로 연결
+server.listen(3000, () => {
+    console.log("3000 listening ")
+});
